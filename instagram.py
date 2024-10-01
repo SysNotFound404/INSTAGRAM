ae74013c469f551fce05089c8d1e4fffa31a88b3
wanz_xcode <rdwn.nd6@gmail.com>
2024-10-01 18:11:50 +0700

    instagram.py

--- instagram.py
+++ instagram.py
@@ -1,1 +1,13 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+try:
+    from Crypto.Cipher import AES
+    from Crypto.Random import get_random_bytes
+    from Crypto.Protocol.KDF import scrypt
+except ImportError:
+    print("pycryptodome tidak ditemukan. Menginstal...")
+    install("pycryptodome")
 import Instagram
